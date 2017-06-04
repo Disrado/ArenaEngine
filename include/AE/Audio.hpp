a5d202f826a9d3800b8df0e22a30e07281798bfa
Disrado <n.maltsev.dis@gmail.com>
2017-06-05 01:26:18 +0300

    sf:: -> ae::, Log class, SFML -> AE

---
+++ include/AE/Audio.hpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2017 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_AUDIO_HPP
+#define SFML_AUDIO_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include <AE/System.hpp>
+#include <AE/Audio/InputSoundFile.hpp>
+#include <AE/Audio/Listener.hpp>
+#include <AE/Audio/Music.hpp>
+#include <AE/Audio/OutputSoundFile.hpp>
+#include <AE/Audio/Sound.hpp>
+#include <AE/Audio/SoundBuffer.hpp>
+#include <AE/Audio/SoundBufferRecorder.hpp>
+#include <AE/Audio/SoundFileFactory.hpp>
+#include <AE/Audio/SoundFileReader.hpp>
+#include <AE/Audio/SoundFileWriter.hpp>
+#include <AE/Audio/SoundRecorder.hpp>
+#include <AE/Audio/SoundSource.hpp>
+#include <AE/Audio/SoundStream.hpp>
+
+
+#endif // SFML_AUDIO_HPP
+
+////////////////////////////////////////////////////////////
+/// \defgroup audio Audio module
+///
+/// Sounds, streaming (musics or custom sources), recording,
+/// spatialization.
+///
+////////////////////////////////////////////////////////////
---
+++ include/AE/System.hpp
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2017 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_SYSTEM_HPP
+#define SFML_SYSTEM_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include <AE/Config.hpp>
+#include <AE/System/Clock.hpp>
+#include <AE/System/Err.hpp>
+#include <AE/System/FileInputStream.hpp>
+#include <AE/System/InputStream.hpp>
+#include <AE/System/Lock.hpp>
+#include <AE/System/MemoryInputStream.hpp>
+#include <AE/System/Mutex.hpp>
+#include <AE/System/NonCopyable.hpp>
+#include <AE/System/Sleep.hpp>
+#include <AE/System/String.hpp>
+#include <AE/System/Thread.hpp>
+#include <AE/System/ThreadLocal.hpp>
+#include <AE/System/ThreadLocalPtr.hpp>
+#include <AE/System/Time.hpp>
+#include <AE/System/Utf.hpp>
+#include <AE/System/Vector2.hpp>
+#include <AE/System/Vector3.hpp>
+
+#endif // SFML_SYSTEM_HPP
+
+////////////////////////////////////////////////////////////
+/// \defgroup system System module
+///
+/// Base module of SFML, defining various utilities. It provides
+/// vector classes, Unicode strings and conversion functions,
+/// threads and mutexes, timing classes.
+///
+////////////////////////////////////////////////////////////
